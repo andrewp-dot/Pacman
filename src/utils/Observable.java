af7f12454b8ab588a1b511c263a74986fe681413
myval2012 <vranaondrej@email.cz>
2023-05-02 15:49:30 +0200

    Observer, Observable

---
+++ src/utils/Observable.java
@@ -0,0 +1,26 @@
+package utils;
+
+/**
+ * Common interface for Observable object.
+ */
+public interface Observable {
+    /**
+     * Adds {@link Observer} to list of observers.
+     * @param observer Observer to add.
+     */
+    void addObserver(Observer observer);
+
+    /**
+     * Removes {@link Observer} from the list of observers.
+     * If observer is not in the list, returns false.
+     *
+     * @param observer Observer to remove.
+     * @return True if observer was successfully removed, false otherwise.
+     */
+    boolean removeObserver(Observer observer);
+
+    /**
+     * Calls update method on all {@link Observer Observers} in the list.
+     */
+    void notifyObservers();
+}
---
+++ src/utils/Observer.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ * Common interface for Observer.
+ */
+public interface Observer {
+    /**
+     * Updates given object.
+     *
+     * @param obj Object to update.
+     */
+    void update(Object obj);
+}
